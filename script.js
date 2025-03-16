dccc924d55afd2cbca67a7f18c642913053915ac
JOSEA2990 <joseamartinezg.2990@gmail.com>
2025-03-16 14:03:30 -0500

    Update13
    Menu Desplegable

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+window.onclick = function(event) {
+    if (!event.target.matches('#Proyectos')) {
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === 'block') {
+                openDropdown.style.display = 'none';
+            }
+        }
+    }
+}
+
 document.getElementById('cambiarPestaña').addEventListener('click', function() {
     window.open('index2.html');
     window.close('index.html');
